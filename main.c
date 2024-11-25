0b81997f6e890a12e137092d96df1c43b9a998a1
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2024-11-25 14:35:11 -0600

    not fork bombing

---
+++ main.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>
+
+#define NUM_CHILDREN 2
+
+
+int main() {
+	pid_t children[NUM_CHILDREN];
+	//Make an array of child pids
+	//In the child pid, the current pid will be zero
+	for (int i=0; i<NUM_CHILDREN; i++) {
+		if (children[i] == 0) {
+			break;
+		}
+		if (children[i] != 0) {
+			children[i] = fork();
+		}
+	}
+	printf("Hello from process");
+	return 0;
+}
